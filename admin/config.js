cbdbe88d8e87edbd7af4ce2ddc446c87e0aa4ef1
Jeremiah Blakely <jeremiah.blakely@gmail.com>
2025-09-19 14:52:04 -0500

    chore: remove legacy admin bundle and audits

--- admin/config.js
+++
@@ -1,6 +0,0 @@
-// Admin configuration
-window.BC_ADMIN_CONFIG = {
-    password: 'admin123',
-    email: 'admin@blakelycinematics.com',
-    name: 'Admin'
-};
--- archive/2025-09-09-pre-cleanup/fix_toggle.js
+++
@@ -1,3 +0,0 @@
-// This file contains the fixes we need to apply
-// Fix 1: Toggle selection on click
-// Fix 2: Prevent rating from selecting
